d9a64f29ab47f242ef0282659ae6e3e1028d072f
anish1985cbe@gmail.com <anish1985cbe@gmail.com>
2024-12-29 22:44:42 +0530

    Varaible has been creaded for aws

---
+++ vars.tf
@@ -0,0 +1,10 @@
+#Varaible
+variable "aws_access_key"{
+ type =string
+ description ="Enter the your access key"
+}
+
+variable "aws_secret_key"{
+ type =string
+ description ="Enter the your access key"
+}
