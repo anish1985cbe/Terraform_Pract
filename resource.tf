191a4b6b4449e2880688ccb0b8c937c40ecb42c7
anish1985cbe@gmail.com <anish1985cbe@gmail.com>
2024-12-29 23:18:53 +0530

    Subnet For Backend created

--- resource.tf
+++ resource.tf
@@ -21,3 +21,17 @@ resource "aws_subnet" "login-front-subnet" {
     Name = "login-frontend-subnet"
   }
 }
+
+
+# Subnet For Backend
+resource "aws_subnet" "login-Backend-subnet" {
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
+
+
+  tags = {
+    Name = "login-Backend-subnet"
+  }
+}
