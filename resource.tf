f469501966d816184f94074a18c85a68266ec759
anish1985cbe@gmail.com <anish1985cbe@gmail.com>
2024-12-29 23:17:12 +0530

    Subnet For Frontend created

---
+++ resource.tf
@@ -0,0 +1,23 @@
+# VPC
+resource "aws_vpc" "login-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "login-vpc"
+  }
+}
+
+
+# Subnet For Frontend
+resource "aws_subnet" "login-front-subnet" {
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
+
+
+  tags = {
+    Name = "login-frontend-subnet"
+  }
+}
